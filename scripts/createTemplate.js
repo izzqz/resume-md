f10bda9fe2dc4c27621861551b8510f47511e4dd
ZZQ <ZZGaming@list.ru>
2021-06-21 01:51:17 +1000

    Clean up

--- scripts/createTemplate.js
+++ scripts/createTemplate.js
@@ -2,7 +2,7 @@ const
     prompts = require('prompts'),
     fs = require('fs-extra'),
     kleur = require('kleur'),
-    { paramCase: toKebabCase } = require("change-case")
+    { paramCase: toKebabCase } = require('change-case');
 
 const questions = [
   {
@@ -25,18 +25,19 @@ const questions = [
 (async () => {
   const metadata = await prompts(questions);
 
-  let templatePath = `templates/${toKebabCase(metadata.name)}`
+  let templatePath = `templates/${toKebabCase(metadata.name)}`;
 
-  fs.copySync('templates/raw', templatePath)
+  fs.copySync('templates/raw', templatePath);
   fs.writeJson(`${templatePath}/metadata.json`, metadata, {
-    spaces: 2
-  })
+    spaces: 2,
+  });
 
   console.log(kleur.green('All done!'));
 
   console.log(metadata);
   console.log('---');
   console.log(`Template path: ${templatePath}`);
-  console.log('Any meta tag changes in:', `${templatePath}/${kleur.cyan('metadata.json')}`);
+  console.log('Meta file:',
+      `${templatePath}/${kleur.cyan('metadata.json')}`);
 })();
 
