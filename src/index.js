08d7c7244e2297133b405086ebea51a9862db1b0
ZZQ <ZZGaming@list.ru>
2021-06-20 23:27:38 +1000

    Add env for dev

--- src/index.js
+++ src/index.js
@@ -10,18 +10,30 @@ const { generateQuestions, generateHtmlResume } = require('./generating.js');
 
 (async () => {
   const mdResume = getMdResume();
-  const templates = getAllTemplates();
-  const questions = generateQuestions(templates);
-  const { template } = await prompts(questions);
+  const allTemplates = getAllTemplates();
 
-  const htmlResume = generateHtmlResume(template, await mdResume);
+  const alreadySelectedIndex = process.env.TEMPLATE_INDEX;
+  const needOpenUrl =
+      process.env.OPEN_URL === 'true' ||
+      process.env.OPEN_URL === undefined;
 
-  if (!fs.existsSync('dist')) fs.mkdirSync('dist')
+  let selectedTemplate;
+
+  if (alreadySelectedIndex === undefined) {
+    const questions = generateQuestions(allTemplates);
+    selectedTemplate = await prompts(questions).then(a => a.template);
+  } else {
+    selectedTemplate = allTemplates[alreadySelectedIndex];
+  }
+
+  const htmlResume = generateHtmlResume(selectedTemplate, await mdResume);
+
+  if (!fs.existsSync('dist')) fs.mkdirSync('dist');
 
   fs.writeFileSync('dist/resume.html', await htmlResume);
   const resumePath = path.resolve('dist/resume.html');
   console.log(`${chalk.green('All done! 🎉')}`);
   console.log('Your resume path:');
   console.log(`\n\n  ${resumePath}\n\n`);
-  openUrl(`file:///${resumePath}`);
+  if (needOpenUrl) openUrl(`file:///${resumePath}`);
 })();
