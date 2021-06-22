55e4c6aca022eaa5c72b91eb34aaa528740aba0a
ZZQ <ZZGaming@list.ru>
2021-06-22 17:41:01 +1000

    Add Promise.all

--- src/index.js
+++ src/index.js
@@ -1,45 +1,46 @@
 const
     prompts = require('prompts'),
     kleur = require('kleur'),
-    { open: openUrl } = require('openurl'),
+    {open: openUrl} = require('openurl'),
     fs = require('fs-extra'),
     path = require('path');
 
-const { getAllTemplates, getMdResume } = require('./parsing.js');
+const {getAllTemplates, getMdResume} = require('./parsing.js');
 const {
-  generateQuestions,
-  generateHtmlContents,
-  generatePdfContents,
+    generateQuestions,
+    generateHtmlContents,
+    generatePdfContents
 } = require('./generating.js');
 
 (async () => {
-  const mdResume = await getMdResume();
-  const allTemplates = getAllTemplates();
+    const mdResume = await getMdResume();
+    const allTemplates = getAllTemplates();
 
-  const alreadySelectedIndex = process.env.TEMPLATE_INDEX;
-  const needOpenUrl =
-      process.env.OPEN_URL === 'true' ||
-      process.env.OPEN_URL === undefined;
+    const alreadySelectedIndex = process.env.TEMPLATE_INDEX;
+    const needOpenUrl =
+        process.env.OPEN_URL === 'true' ||
+        process.env.OPEN_URL === undefined;
 
-  let selectedTemplate;
+    let selectedTemplate;
 
-  if (alreadySelectedIndex === undefined) {
-    const questions = generateQuestions(allTemplates);
-    selectedTemplate = await prompts(questions).then(a => a.template);
-  } else {
-    selectedTemplate = allTemplates[alreadySelectedIndex];
-  }
+    if (alreadySelectedIndex === undefined) {
+        const questions = generateQuestions(allTemplates);
+        selectedTemplate = await prompts(questions).then(a => a.template);
+    } else {
+        selectedTemplate = allTemplates[alreadySelectedIndex];
+    }
 
-  // FIXME: shitty pdf font and css usages
-  const htmlContent = await generateHtmlContents(selectedTemplate, mdResume);
-  const pdfContent = await generatePdfContents(htmlContent)
+    // FIXME: shitty pdf font and css usages
+    const html = await generateHtmlContents(selectedTemplate, mdResume);
 
-  await fs.outputFile('dist/resume.html', htmlContent);
-  await fs.outputFile('dist/resume.pdf', pdfContent);
+    await Promise.all([
+        generatePdfContents(html).then(pdf => fs.outputFile('dist/resume.pdf', pdf)),
+        fs.outputFile('dist/resume.html', html)
+    ]);
 
-  const resumePath = path.resolve('dist/resume.html');
-  console.log(`${kleur.green('All done! 🎉')}`);
-  console.log('Your resume path:');
-  console.log(`\n\n  ${resumePath}\n\n`);
-  if (needOpenUrl) openUrl(`file:///${resumePath}`);
+    const resumePath = path.resolve('dist/resume.html');
+    console.log(`${kleur.green('All done! 🎉')}`);
+    console.log('Your resume path:');
+    console.log(`\n\n  ${resumePath}\n\n`);
+    if (needOpenUrl) openUrl(`file:///${resumePath}`);
 })();
